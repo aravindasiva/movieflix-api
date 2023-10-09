db7eff04c7379168a861af5338b2c84d501e5882
aravindasiva <aravindcva@hotmail.com>
2023-10-09 15:53:58 +0100

    add request logging

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_FILTER } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MoviesModule } from './movies/movies.module';
 import { GenresModule } from './genres/genres.module';
+import { NotFoundExceptionFilter } from './exceptions/not-found-exception.filter';
+import { RequestLoggingMiddleware } from './middleware/request-logging.middleware';
 
 @Module({
   imports: [
@@ -14,5 +17,17 @@ import { GenresModule } from './genres/genres.module';
     GenresModule,
     MoviesModule,
   ],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: NotFoundExceptionFilter,
+    },
+  ],
 })
-export class AppModule { }
+// export class AppModule { }
+
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggingMiddleware).forRoutes('*');
+  }
+}
