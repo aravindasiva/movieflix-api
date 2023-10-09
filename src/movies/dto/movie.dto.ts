165d129f5138f589f4ba59705f4310539645a948
aravindasiva <aravindcva@hotmail.com>
2023-10-09 15:56:21 +0100

    add validators

--- src/movies/dto/movie.dto.ts
+++ src/movies/dto/movie.dto.ts
@@ -1,14 +1,21 @@
+import { IsString, IsDate } from "class-validator";
+
 export class CreateMovieDto {
+    @IsString()
     title: string;
+
+    @IsString()
     description: string;
+
+    @IsDate()
     releaseDate: Date;
+
     genre: string[];
-  }
-  
-  export class UpdateMovieDto {
+}
+
+export class UpdateMovieDto {
     title?: string;
     description?: string;
     releaseDate?: Date;
     genre?: string[];
-  }
-  
+}
